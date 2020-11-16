8ea6556f1949ce7099d1f4003dba744965cce881
Hammad Ahmed <shammad.c5@hotmail.com>
2020-11-16 16:33:44 +0000

    format the js file a little bit

--- script.js
+++ script.js
@@ -1,172 +1,166 @@
+const gameBoard = (() => {
+  let boxes = [];
 
-		const gameBoard = (() => {
+  const getBoxes = () => boxes;
 
-			let boxes = [];
+  const addBox = (box, index) => boxes[index] = box;
 
-			const getBoxes = () => boxes;
-
-			const addBox = (box, index) => boxes[index] = box;
-
-			const reintialize = () => {
-				boxes = [];
-				document.querySelectorAll('.box').forEach(box => {
-					box.textContent = '';
-					box.style.backgroundColor = '#d1d8e0';
-				});
+  const reintialize = () => {
+    boxes = [];
+    document.querySelectorAll('.box').forEach(box => {
+      box.textContent = '';
+      box.style.backgroundColor = '#d1d8e0';
+    });
 
 
-				displayController.clearMessageBox();
-			};
+    displayController.clearMessageBox();
+  };
 
-			return { getBoxes, addBox, reintialize };
+  return { getBoxes, addBox, reintialize };
+})();
 
-		})();
 
-		const player = () => {
 
-			return {};
-		};
+const player = () => {
+  return {};
+};
 
-		const gamePlay = (() => {
+const gamePlay = (() => {
+  let currentPlayer = 'x',
+    over = false,
+    winningCondition;
 
-			let currentPlayer = 'x',
-				over = false,
-				winningCondition;
+  const winConditions = [
+    [0,1,2], [3,4,5], [6,7,8],
+    [0,3,6], [1,4,7], [2,5,8],
+    [0,4,8], [2,4,6]
+  ];
 
-			const winConditions = [
-				[0,1,2], [3,4,5], [6,7,8],
-				[0,3,6], [1,4,7], [2,5,8],
-				[0,4,8], [2,4,6]
-			];
+  const isOver = () => over;
 
-			const isOver = () => over;
+  const changeCurrentPlayer = () => currentPlayer = currentPlayer === 'x' ? 'o' : 'x';
 
-			const changeCurrentPlayer = () => currentPlayer = currentPlayer === 'x' ? 'o' : 'x';
+  const listenClicks = () => {
+    document.querySelectorAll('.box').forEach(box => {
+      box.addEventListener('click', () => {
+        if (!box.textContent && !over) {
+          box.textContent = currentPlayer;
+          gameBoard.addBox(currentPlayer, box.dataset.box);
 
-			const listenClicks = () => {
-				document.querySelectorAll('.box').forEach(box => {
-					box.addEventListener('click', () => {
-						if (!box.textContent && !over) {
-							box.textContent = currentPlayer;
-							gameBoard.addBox(currentPlayer, box.dataset.box);
+          checkIsOver();
 
-							checkIsOver();
+          if(isOver()) {
+            displayController.highlight(winningCondition);
+            displayController.congratulate(currentPlayer);
+          }
 
-							if(isOver()) {
-								displayController.highlight(winningCondition);
-								displayController.congratulate(currentPlayer);
-							}
+          changeCurrentPlayer();
+        }
+      });
+    });
+  }
 
-							changeCurrentPlayer();
-						}
-					});
-				});
-			}
+  const checkIsOver = () => {
+    let boxes = gameBoard.getBoxes();
 
-			const checkIsOver = () => {
-				let boxes = gameBoard.getBoxes();
+    winConditions.some(c => {
+      if (boxes[c[0]] === boxes[c[1]] && boxes[c[1]] === boxes[c[2]] && boxes[c[1]]) {
+        over = true;
+        winningCondition = c;
 
-				winConditions.some(c => {
-					if (boxes[c[0]] === boxes[c[1]] && boxes[c[1]] === boxes[c[2]] && boxes[c[1]]) {
-						over = true;
-						winningCondition = c;
+        return true;
+      }
+    });
+  };
 
-						return true;
-					}
-				});
-			};
+  const restart = () => {
+    currentPlayer = 'x',
+    over = false,
+    winningCondition = undefined;
 
-			const restart = () => {
-				currentPlayer = 'x',
-				over = false,
-				winningCondition = undefined;
+    gameBoard.reintialize();
+  };
 
-				gameBoard.reintialize();
-			};
+  return { listenClicks, restart };
+})();
 
-			return { listenClicks, restart };
+const displayController = (() => {
+  const game = document.getElementById('game');
 
-		})();
+  const highlight = condition => {
+    boxesToHighlight = [];
 
-		const displayController = (() => {
+    condition.forEach(index => {
+      boxesToHighlight.push(document.querySelector(`.box[data-box="${index}"]`));
+    });
 
-			const game = document.getElementById('game');
+    boxesToHighlight.forEach(box => box.style.backgroundColor = '#26de81')
+  };
 
-			const highlight = condition => {
-				boxesToHighlight = [];
+  const congratulate = player => {
+    document.getElementById('messageBox').textContent = `${player.toUpperCase()} has won!`;
+  }
 
-				condition.forEach(index => {
-					boxesToHighlight.push(document.querySelector(`.box[data-box="${index}"]`));
-				});
+  const clearMessageBox = () => document.getElementById('messageBox').textContent = ``;
 
-				boxesToHighlight.forEach(box => box.style.backgroundColor = '#26de81')
-			};
+  const addNames = e => {
+    e.preventDefault();
+    let firstPlayer = document.getElementById('first').value;
+    let secondPlayer = document.getElementById('second').value;
+    document.getElementById('messageBox').textContent = `${firstPlayer} is 'X' and ${secondPlayer} is 'O'`;
+    document.getElementById('addNamesButton').textContent = 'Change names';
+  };
 
-			const congratulate = player => {
-				document.getElementById('messageBox').textContent = `${player.toUpperCase()} has won!`;
-			}
+  const render = () => {
+    board = document.createElement('section')
+    board.setAttribute('id', 'board');
 
-			const clearMessageBox = () => document.getElementById('messageBox').textContent = ``;
+    for (let i = 0; i <= 8; i++) {
 
-			const addNames = e => {
-				e.preventDefault();
-				let firstPlayer = document.getElementById('first').value;
-				let secondPlayer = document.getElementById('second').value;
-				document.getElementById('messageBox').textContent = `${firstPlayer} is 'X' and ${secondPlayer} is 'O'`;
-				document.getElementById('addNamesButton').textContent = 'Change names';
-			};
+      let span = document.createElement('span');
+      span.classList.add('box');
+      span.dataset.box = i;
 
-			const render = () => {
-				board = document.createElement('section')
-				board.setAttribute('id', 'board');
+      board.appendChild(span);
+    }
 
-				for (let i = 0; i <= 8; i++) {
+    restartButton = document.createElement('button')
+    restartButton.setAttribute('id', 'restart');
+    restartButton.textContent = 'Restart';
+    restartButton.addEventListener('click', gamePlay.restart);
 
-					let span = document.createElement('span');
-					span.classList.add('box');
-					span.dataset.box = i;
+    messageBox = document.createElement('p');
+    messageBox.setAttribute('id', 'messageBox');
+    messageBox.textContent;
 
-					board.appendChild(span);
-				}
+    namesForm = document.createElement('form');
 
-				restartButton = document.createElement('button')
-				restartButton.setAttribute('id', 'restart');
-				restartButton.textContent = 'Restart';
-				restartButton.addEventListener('click', gamePlay.restart);
+    firstNameInput = document.createElement('input');
+    firstNameInput.setAttribute('id', 'first');
+    firstNameInput.setAttribute('placeholder', 'First player\'s name');
 
-				messageBox = document.createElement('p');
-				messageBox.setAttribute('id', 'messageBox');
-				messageBox.textContent;
+    secondNameInput = document.createElement('input');
+    secondNameInput.setAttribute('id', 'second');
+    secondNameInput.setAttribute('placeholder', 'Second player\'s name');
 
-				namesForm = document.createElement('form');
+    addNamesButton = document.createElement('button');
+    addNamesButton.setAttribute('id', 'addNamesButton');
+    addNamesButton.textContent = 'Add names';
+    addNamesButton.addEventListener('click', addNames);
 
-				firstNameInput = document.createElement('input');
-				firstNameInput.setAttribute('id', 'first');
-				firstNameInput.setAttribute('placeholder', 'First player\'s name');
+    namesForm.appendChild(firstNameInput);
+    namesForm.appendChild(secondNameInput);
+    namesForm.appendChild(addNamesButton);
 
-				secondNameInput = document.createElement('input');
-				secondNameInput.setAttribute('id', 'second');
-				secondNameInput.setAttribute('placeholder', 'Second player\'s name');
+    game.appendChild(restartButton);
+    game.appendChild(namesForm);
+    game.appendChild(messageBox);
+    game.appendChild(board);
+  }
 
-				addNamesButton = document.createElement('button');
-				addNamesButton.setAttribute('id', 'addNamesButton');
-				addNamesButton.textContent = 'Add names';
-				addNamesButton.addEventListener('click', addNames);
+  return { render, highlight, congratulate, clearMessageBox };
+})();
 
-				namesForm.appendChild(firstNameInput);
-				namesForm.appendChild(secondNameInput);
-				namesForm.appendChild(addNamesButton);
+displayController.render();
 
-				game.appendChild(restartButton);
-				game.appendChild(namesForm);
-				game.appendChild(messageBox);
-				game.appendChild(board);
-			}
-
-			return { render, highlight, congratulate, clearMessageBox };
-
-		})();
-
-		displayController.render();
-
-		gamePlay.listenClicks();
+gamePlay.listenClicks();
